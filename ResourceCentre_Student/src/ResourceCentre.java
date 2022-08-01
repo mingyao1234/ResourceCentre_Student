54503b39a3935bc408eec1024749eecb7bc8d531
mingyao1234 <21023135@myrp.edu.sg>
2022-08-01 15:50:48 +0800

    e

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -136,7 +136,7 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%-84s \n", camcorderList.get(i).toString());
+			output += String.format("%-84s \n", camcorderList.get(i).toString()); //duplicate code fixed
 		}
 		return output;
 	}
